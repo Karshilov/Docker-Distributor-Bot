05901cd6d481a90ef000d8d501ed1cde0f1737cf
Karshilov <karshilov@foxmail.com>
2021-10-19 23:55:51 +0800

    chore: remove useless test output & fuck u windows

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"Docker-Distributor-Bot/pkg/dockerClient"
-	"os"
 	"path"
 
 	"github.com/docker/docker/pkg/archive"
 )
 
 func main() {
-	pwd, _ := os.Getwd()
-	println(path.Join(pwd, "utils/nvidia-cuda"))
 	tar, err := archive.TarWithOptions(path.Join("utils/nvidia-cuda"), &archive.TarOptions{})
 	if err != nil {
 		panic(err)
