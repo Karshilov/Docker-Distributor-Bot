49e8215900d3669d435775be445d5ad8c04c3f3f
Karshilov <karshilov@foxmail.com>
2021-10-20 21:04:16 +0800

    feat: part of create container

--- pkg/dockerClient/createContainer.go
+++ pkg/dockerClient/createContainer.go
@@ -1,26 +1,58 @@
 package dockerClient
 
 import (
+	"Docker-Distributor-Bot/utils/config"
+	"Docker-Distributor-Bot/utils/random"
 	"context"
+	"fmt"
+	"net"
+	"strconv"
 	"time"
 
-	"Docker-Distributor-Bot/utils/random"
-
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/go-connections/nat"
 )
+
+func GetAvailablePort(host string) (int, error) {
+	address, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:0", host))
+	if err != nil {
+		return 0, err
+	}
+	listener, err := net.ListenTCP("tcp", address)
+	if err != nil {
+		return 0, err
+	}
+	defer listener.Close()
+	return listener.Addr().(*net.TCPAddr).Port, nil
+}
 
 func createContainer(hostId uint, pubKey string, qqnum int64) error {
 	cli, err := getDockerClient(hostId)
+	hostInfo := config.GetConfig()[hostId]
 	if err != nil {
 		return err
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
 	defer cancel()
+	passwd := random.GetRandomPassword()
+	port, err := GetAvailablePort(hostInfo.Host)
+	if err != nil {
+		return err
+	}
 	cfg := &container.Config{
 		Env: []string{
-			"ROOT_PASSWORD=" + random.GetRandomPassword(),
+			"ROOT_PASSWORD=" + passwd,
 			"AUTHORIZED_KEYS=" + pubKey,
 		},
+	}
+	hostCfg := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"2222/tcp": []nat.PortBinding{nat.PortBinding{
+				HostIP:   hostInfo.Host,
+				HostPort: strconv.Itoa(port),
+			}},
+		},
+		Binds: []string{fmt.Sprintf("/var/%d-%d/persist-data", qqnum, 0)},
 	}
 	return nil
 }
--- utils/random/randomPassword.go
+++ utils/random/randomPassword.go
@@ -1,6 +1,17 @@
 package random
 
+import (
+	"bytes"
+	"math/rand"
+	"time"
+)
+
 func GetRandomPassword() string {
-	passwd := ""
-	return passwd
+	const charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*"
+	rand.NewSource(time.Now().UnixNano())
+	var s bytes.Buffer
+	for i := 0; i < 14; i++ {
+		s.WriteByte(charSet[rand.Int63()%int64(len(charSet))])
+	}
+	return s.String()
 }
